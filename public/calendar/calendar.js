3a3f5dc88aa92e43faa328a974e3539bc18847a8
ste-spa <steve.spatzek@gmail.com>
2023-11-02 12:20:43 +0100

    calendar.js

--- public/calendar/calendar.js
+++ public/calendar/calendar.js
@@ -1,80 +1,42 @@
-import FullCalendar from "";
+function getCalendarEvents(info, successCallback, failureCallback) {
+  
+  $.ajax({
+  type: 'GET',
+  url: 'https://dhbw-appointment-scheduler-ad7e04c77a13.herokuapp.com/api/v1.0/appointments/all?calendar',
+  dataType: 'json',
+  success: function(data) {
+    var events = [];
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      var appointment = data[i];
 
-document.addEventListener("DOMContentLoaded", function () {
-  var calendarEl = document.getElementById("calendar");
+      var datetimeString = appointment.date + 'T' + appointment.time;
+      var startDateTime = new Date(datetimeString);
+      var durationParts = appointment.duration.split(':');
+      var hours = parseInt(durationParts[0], 10);
+      var minutes = parseInt(durationParts[1], 10);
 
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialDate: "2023-01-12",
-    initialView: "timeGridWeek",
-    nowIndicator: true,
-    headerToolbar: {
-      left: "prev,next today",
-      center: "title",
-      right: "dayGridMonth,timeGridWeek,timeGridDay",
-    },
-    navLinks: true, // can click day/week names to navigate views
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: true, // allow "more" link when too many events
-    events: [
-      {
-        title: "All Day Event",
-        start: "2023-01-01",
-      },
-      {
-        title: "Long Event",
-        start: "2023-01-07",
-        end: "2023-01-10",
-      },
-      {
-        groupId: 999,
-        title: "Repeating Event",
-        start: "2023-01-09T16:00:00",
-      },
-      {
-        groupId: 999,
-        title: "Repeating Event",
-        start: "2023-01-16T16:00:00",
-      },
-      {
-        title: "Conference",
-        start: "2023-01-11",
-        end: "2023-01-13",
-      },
-      {
-        title: "Meeting",
-        start: "2023-01-12T10:30:00",
-        end: "2023-01-12T12:30:00",
-      },
-      {
-        title: "Lunch",
-        start: "2023-01-12T12:00:00",
-      },
-      {
-        title: "Meeting",
-        start: "2023-01-12T14:30:00",
-      },
-      {
-        title: "Happy Hour",
-        start: "2023-01-12T17:30:00",
-      },
-      {
-        title: "Dinner",
-        start: "2023-01-12T20:00:00",
-      },
-      {
-        title: "Birthday Party",
-        start: "2023-01-13T07:00:00",
-      },
-      {
-        title: "Click for Google",
-        url: "http://google.com/",
-        start: "2023-01-28",
-      },
-    ],
-  });
+      var endDateTime = new Date(startDateTime);
+      endDateTime.setHours(startDateTime.getHours() + hours);
+      endDateTime.setMinutes(startDateTime.getMinutes() + minutes);
 
-  calendar.render();
-  calendar.setOption("locale", "de");
+      events.push({
+        title: "Nicht verfügbar",
+        start: startDateTime,
+        end: endDateTime,
+        status: "booked"
+      });
+    }
+
+    // Hier werden die Events zur Kalenderansicht hinzugefügt
+    calendar.addEventSource(events);
+
+    // Kalender rendern nachdem die Daten geladen wurden
+    calendar.render();
+  },
+  error: function() {
+    // Bei einem Fehler kannst du hier eine Fehlerbehandlung implementieren
+    console.error('Fehler beim Abrufen der Daten');
+  }
 });
+}
